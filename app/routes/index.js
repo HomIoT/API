bb0995144df50d84e06326520d8e748d3dbdbd22
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-05-13 00:51:29 +0330

    Remove JWT

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,12 @@ import express from "express";
 
 import SystemRoutes from "$routes/system/system.routes.js";
 import AuthRoutes from "$routes/auth/auth.routes.js";
+
 import { jwt } from "$middlewares/index.js";
 
 const Router = express();
 
-Router.use("/systems", jwt, SystemRoutes);
+Router.use("/systems", SystemRoutes);
 Router.use("/auth", AuthRoutes);
 
 export default Router;
